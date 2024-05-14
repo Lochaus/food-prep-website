25b2eae35bf476fd5f8547a9f6da29c8c8d7beba
Lochlan Walshe <33631368+Lochaus@users.noreply.github.com>
2024-05-14 17:32:11 +0100

    Servings Adjuster
    Working on adjusting servings

--- dataFiller.js
+++ dataFiller.js
@@ -1,22 +1,42 @@
 const listEl = document.querySelector("#ingredients-fieldset")
-var recipe = document.getElementById("data-input").getAttribute("recipe-name")
-
-fetch('../recipes.json')
-    .then(res => res.json())
-    .then(data => {
-        for (let i = 0; i < data[recipe].ingredients.length; i++) {
-            listEl.insertAdjacentHTML("beforeend", `<div><input type="checkbox"><span>${data[recipe].quantities[i]} ${data[recipe].unit[i]} ${data[recipe].ingredients[i]}</span></div>`)
-        }
-    }
-    )
-
 const listEl1 = document.querySelector("#instructions-list")
+var recipe = document.getElementById("data-input").getAttribute("recipe-name")
+var normalServings = document.getElementById("servings").innerText
+var desiredServings = document.getElementById("servings").innerText
+console.log(servings)
 
-fetch('../recipes.json')
-    .then(res => res.json())
-    .then(data => {
-        for (let i = 0; i < data[recipe].instructions.length; i++) {
-            listEl1.insertAdjacentHTML("beforeend", `<li>${data[recipe].instructions[i]}</li>`)
+
+window.onload = function () {
+    loadRecipe(desiredServings)
+}
+
+function loadRecipe(desiredServings) {
+    desiredServings = desiredServings
+    listEl.innerHTML = ""
+    listEl1.innerHTML = ""
+    fetch('../recipes.json')
+        .then(res => res.json())
+        .then(data => {
+            for (let i = 0; i < data[recipe].ingredients.length; i++) {
+                listEl.insertAdjacentHTML("beforeend", `<div><input type="checkbox"><span>${data[recipe].quantities[i] * (desiredServings / normalServings)} ${data[recipe].unit[i]} ${data[recipe].ingredients[i]}</span></div>`)
+            }
         }
-    }
-    )
+        )
+
+
+    fetch('../recipes.json')
+        .then(res => res.json())
+        .then(data => {
+            for (let i = 0; i < data[recipe].instructions.length; i++) {
+                listEl1.insertAdjacentHTML("beforeend", `<li>${data[recipe].instructions[i]}</li>`)
+            }
+        }
+        )
+}
+
+var form = document.getElementById('form');
+number = document.getElementById('number');
+form.onsubmit = function () {
+    desiredServings = number.value;
+    loadRecipe(4)
+}
